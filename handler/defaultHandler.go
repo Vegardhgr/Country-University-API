416248e57917dc408daeb05b20c87b0bab7d2505
Vegardhgr <vegard.groder@gmail.com>
2023-03-04 16:41:58 +0100

    Changed the output string in defaultHandler.

--- handler/defaultHandler.go
+++ handler/defaultHandler.go
@@ -10,10 +10,14 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "text/html")
 
 	// Offer information for redirection to paths
-	output := "This service does not provide any functionality on root path level. Please use the paths below\n" +
-		"<a href=\"" + UNI_INFO_PATH + "\">" +
-		"<a href=\"" + NEIGHBOUR_UNIS_PATH + "\">" +
-		"<a href=\"" + DIAG_PATH + "\">"
+	output := "<h1>Welcome!<h1><br><h3>This service does not provide any functionality on root path level." +
+		" Please try one of the paths below<h3><br><br>" +
+		"<h6>Search by university:<h6><br>" +
+		"<a href=\"" + UNI_INFO_PATH + "\">" + UNI_INFO_PATH + "</a><br><br>" +
+		"<h6>Search for universities in a neighbouring country:<h6><br>" +
+		"<a href=\"" + NEIGHBOUR_UNIS_PATH + "\">" + NEIGHBOUR_UNIS_PATH + "</a><br><br>" +
+		"<h6>For diagnostics:<h6><br>" +
+		"<a href=\"" + DIAG_PATH + "\">" + DIAG_PATH + "</a>"
 
 	// Write output to client
 	_, err := fmt.Fprintf(w, "%v", output)
