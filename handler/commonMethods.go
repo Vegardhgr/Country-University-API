079cb77851ebd15b0dbd1fcfdba0d5afa944d9e4
Vegardhgr <vegard.groder@gmail.com>
2023-03-06 15:30:18 +0100

    commonMethods.go: Removed unnecessary error methods.

--- handler/commonMethods.go
+++ handler/commonMethods.go
@@ -2,30 +2,27 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
 // GetCountryByName
-///*Returns the http response from the country api*/
+//Returns the http response from the country API
 func GetCountryByName(w http.ResponseWriter, countryName string) (*http.Response, bool) {
 	countryInfo, err := http.Get(COUNTRY_URL + "v3.1/name/" + countryName + "?fullText=true")
-	/*Checks if countryInfo is nil. Can be caused by an i/o timeout*/
-	if countryInfo == nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		fmt.Println("countryInfo is nil after getting the country url by name. " +
-			"Can be caused by an i/o timeout")
-		return nil, false
-	}
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		log.Println("Error when getting country by name: ", err)
 		return nil, false
 	}
 	/*Checks the status code for the response*/
-	if countryInfo.StatusCode != 200 {
+	if countryInfo.StatusCode != http.StatusOK {
+		if countryInfo.StatusCode == http.StatusNotFound {
+			/*The country API returns a 404 (http.StatusNotFound) if there is no match
+			for a specific country name.*/
+			return nil, true
+		}
 		http.Error(w, http.StatusText(countryInfo.StatusCode), countryInfo.StatusCode)
 		log.Println("Invalid country name.")
 		return nil, false
@@ -34,29 +31,17 @@ func GetCountryByName(w http.ResponseWriter, countryName string) (*http.Response
 }
 
 // GetCountryByAlphaCode
-///*Returns the http response from the country api*/
+//Returns the http response from the country API
 func GetCountryByAlphaCode(w http.ResponseWriter, alphaCode string) (*http.Response, bool) {
 	countryInfo, err := http.Get(COUNTRY_URL + "v3.1/alpha/" + alphaCode)
-	/*Checks if countryInfo is nil. Can be caused by an i/o timeout*/
-	if countryInfo == nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		fmt.Println("countryInfo is nil after getting the country url by alpha code. " +
-			"Can be caused by an i/o timeout")
-		return nil, false
-	}
-	/*Checks the status code for the response*/
-	if countryInfo.StatusCode != 200 {
-		http.Error(w, http.StatusText(countryInfo.StatusCode), countryInfo.StatusCode)
-		log.Println("Invalid alpha code.")
-		return nil, false
-	}
 	if err != nil {
 		log.Println("Error when getting country by alpha code: ", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return nil, false
 	}
+
 	/*Checks the status code for the response*/
-	if countryInfo.StatusCode != 200 {
+	if countryInfo.StatusCode != http.StatusOK {
 		http.Error(w, http.StatusText(countryInfo.StatusCode), countryInfo.StatusCode)
 		log.Println("Invalid alpha code.")
 		return nil, false
@@ -65,23 +50,16 @@ func GetCountryByAlphaCode(w http.ResponseWriter, alphaCode string) (*http.Respo
 }
 
 // GetUniByName
-///*Returns the http response from the university api*/
+//Returns the http response from the university API
 func GetUniByName(w http.ResponseWriter, universityName string) (*http.Response, bool) {
 	uniInfo, err := http.Get(UNI_URL + "search?name_contains=" + universityName)
-	/*Checks if uniInfo is nil. Can be caused by an i/o timeout*/
-	if uniInfo == nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		fmt.Println("uniInfo is nil after getting the uni url. " +
-			"Can be caused by an i/o timeout")
-		return nil, false
-	}
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		log.Println("Error getting the university url: ", err)
 		return nil, false
 	}
 	/*Checks the status code for the response*/
-	if uniInfo.StatusCode != 200 {
+	if uniInfo.StatusCode != http.StatusOK {
 		http.Error(w, http.StatusText(uniInfo.StatusCode), uniInfo.StatusCode)
 		log.Println("Bad response from the uni api. Status code: ", uniInfo.StatusCode)
 		return nil, false
@@ -90,7 +68,7 @@ func GetUniByName(w http.ResponseWriter, universityName string) (*http.Response,
 }
 
 // Decode
-///*A general function for decoding*/
+//A general function for decoding
 func Decode(w http.ResponseWriter, body io.ReadCloser, list any) bool {
 	err := json.NewDecoder(body).Decode(&list)
 	if err != nil {
@@ -102,7 +80,7 @@ func Decode(w http.ResponseWriter, body io.ReadCloser, list any) bool {
 }
 
 //AddCountryToArr
-///*Adds a country to an array*/
+//Adds a country to an array
 func AddCountryToArr(w http.ResponseWriter, countryCode string, countryArr *[]Country) bool {
 	var tempCountryArr []Country
 
